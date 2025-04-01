8be1a0eea544773da839b4821a5b06b3afef9787
Kamal Sharma <kamals08112002@gmail.com>
2025-04-01 17:41:59 +0530

    util code discussions

--- src/index.js
+++ src/index.js
@@ -1,32 +1,19 @@
 // require('dotenv').config({path:'./env'})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
- dotenv.config({
-    path:"./env"
- })
+dotenv.config({
+  path: "./env",
+});
 
-
- connectDB()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`App is listening on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo DB connection error", err);
+  });
 
 /*
 import express from "express";
