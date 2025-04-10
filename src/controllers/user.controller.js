d1f87164424668981a6bbc89ed725947919cd190
Kamal Sharma <kamals08112002@gmail.com>
2025-04-10 12:09:15 +0530

    fixed all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -15,31 +15,42 @@ const registerUser = asyncHandler(async (req, res) => {
   // check for user creation
   // return response
 
-  const { fullName, email, username, password } = req.body;
+  const { fullname, email, username, password } = req.body;
   console.log("email", email);
 
   if (
-    [fullName, email, username, password].some((item) => item?.trim() === "")
+    [fullname, email, username, password].some((item) => item?.trim() === "")
   ) {
     throw new ApiError(400, "Please fill all the fields");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ email }, { username }],
   });
 
   if (existedUser) {
     throw new ApiError(409, "User already exists with this email or username");
   }
+  console.log("req.files:", req.files);
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Please upload an avatar image");
   }
-
+  console.log("Avatar local path:", avatarLocalPath);
   const avatar = await uploadOnCloudinary(avatarLocalPath);
+  console.log("Avatar upload result:", avatar);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
 
   if (!avatar) {
@@ -47,7 +58,7 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const user = await User.create({
-    fullName,
+    fullname,
     email,
     username: username.toLowerCase(),
     password,
